903cad07a7d9d25299ef4e48c94c18c113f42d34
josh <josh@josh.josh>
2025-12-04 01:58:32 -0600

    Header sticky con botones de acción en edición de cliente

--- app/Filament/Resources/ClienteResource/Pages/EditCliente.php
+++ app/Filament/Resources/ClienteResource/Pages/EditCliente.php
@@ -3,38 +3,21 @@
 namespace App\Filament\Resources\ClienteResource\Pages;
 
 use App\Filament\Resources\ClienteResource;
-use App\Filament\Resources\VelaSportPostResource;
 use Filament\Actions;
 use Filament\Resources\Pages\EditRecord;
 
 class EditCliente extends EditRecord
 {
     protected static string $resource = ClienteResource::class;
+    
+    protected static string $view = 'filament.resources.cliente-resource.pages.edit-cliente';
 
+    // Acción de eliminar para el wire:click
     protected function getHeaderActions(): array
     {
         return [
-            Actions\Action::make('view')
-                ->label('Ver')
-                ->icon('heroicon-o-eye')
-                ->color('success')
-                ->url(fn () => ClienteResource::getUrl('view', ['record' => $this->record])),
-            Actions\Action::make('posts')
-                ->label('Ver Posts')
-                ->icon('heroicon-o-newspaper')
-                ->color('info')
-                ->url(VelaSportPostResource::getUrl('index')),
-            Actions\CreateAction::make()
-                ->label('Nuevo Cliente')
-                ->icon('heroicon-o-plus')
-                ->url(ClienteResource::getUrl('create')),
             Actions\DeleteAction::make()
                 ->label('Eliminar'),
-            Actions\Action::make('back')
-                ->label('Volver')
-                ->icon('heroicon-o-arrow-left')
-                ->color('gray')
-                ->url(ClienteResource::getUrl('index')),
         ];
     }
 }

9d4b49efa224b8a3a41e0403e70f63e8ea681f69
josh <josh@josh.josh>
2025-12-03 23:30:56 -0600

    Agregar seeder para usuario admin

---
+++ database/seeders/AdminUserSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class AdminUserSeeder extends Seeder
+{
+    public function run(): void
+    {
+        User::updateOrCreate(
+            ['email' => 'info@websolutions.work'],
+            [
+                'name' => 'Web Solutions',
+                'email' => 'info@websolutions.work',
+                'password' => Hash::make('marihuana420'),
+            ]
+        );
+    }
+}
+

886228c23429328da23c7188277f9e32cd971e80
josh <josh@josh.josh>
2025-12-04 09:24:26 -0600

    Cambiar Calendario de citas a componentes nativos de Filament

--- app/Filament/Pages/GoogleCalendar.php
+++ app/Filament/Pages/GoogleCalendar.php
@@ -3,17 +3,33 @@
 namespace App\Filament\Pages;
 
 use Filament\Pages\Page;
+use Filament\Actions\Action;
 
 class GoogleCalendar extends Page
 {
     protected static ?string $navigationIcon = 'heroicon-o-calendar-days';
-    protected static ?string $navigationLabel = 'Calendario de citas';
-    protected static ?string $title = 'Google Calendar';
+    protected static ?string $navigationLabel = 'Calendario';
+    protected static ?string $title = 'Calendario';
     protected static ?string $navigationGroup = 'Clientes';
     protected static ?int $navigationSort = 3;
 
     protected static string $view = 'filament.pages.google-calendar';
 
-    // Puedes cambiar este ID por el de tu calendario de Google
-    public string $calendarId = 'es.cr#holiday@group.v.calendar.google.com';
+    protected function getHeaderActions(): array
+    {
+        return [
+            Action::make('nuevo_evento')
+                ->label('Nuevo Evento')
+                ->icon('heroicon-o-plus')
+                ->url('https://calendar.google.com/calendar/r/eventedit')
+                ->openUrlInNewTab()
+                ->color('success'),
+            Action::make('abrir_calendario')
+                ->label('Abrir Google Calendar')
+                ->icon('heroicon-o-arrow-top-right-on-square')
+                ->url('https://calendar.google.com')
+                ->openUrlInNewTab()
+                ->color('gray'),
+        ];
+    }
 }

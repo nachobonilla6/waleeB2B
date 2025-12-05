f877d24fe02129e2a0769dd1f8bbdc4806b47131
josh <josh@josh.josh>
2025-12-05 01:08:37 -0600

    fix: Manejar cliente como string o objeto en ejemplo1-mobile

--- resources/views/ejemplo1-mobile.blade.php
+++ resources/views/ejemplo1-mobile.blade.php
@@ -84,12 +84,19 @@
                                     </svg>
                                     {{ $cita->fecha_inicio->format('d/m/Y H:i') }}
                                 </div>
-                                @if($cita->cliente)
+                                @if($cita->cliente_id && $cita->cliente)
                                     <div class="flex items-center text-xs text-gray-600">
                                         <svg class="w-3 h-3 mr-1" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                                             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z"/>
                                         </svg>
-                                        {{ $cita->cliente->nombre_empresa }}
+                                        {{ is_object($cita->cliente) ? $cita->cliente->nombre_empresa : $cita->cliente }}
+                                    </div>
+                                @elseif($cita->cliente && is_string($cita->cliente))
+                                    <div class="flex items-center text-xs text-gray-600">
+                                        <svg class="w-3 h-3 mr-1" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+                                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z"/>
+                                        </svg>
+                                        {{ $cita->cliente }}
                                     </div>
                                 @endif
                             </div>

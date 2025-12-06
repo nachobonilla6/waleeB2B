cad32f9fbd604cfc6d8460d148e3010f474d2f1f
nachobonilla6 <nachobonilla6@gmail.com>
2025-12-06 01:45:28 -0600

    Agregar import de Schema en ClientResource

--- app/Filament/Resources/ClientResource.php
+++ app/Filament/Resources/ClientResource.php
@@ -13,6 +13,7 @@ use Filament\Tables\Table;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
 use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Schema;
 use Filament\Notifications\Notification;
 
 class ClientResource extends Resource

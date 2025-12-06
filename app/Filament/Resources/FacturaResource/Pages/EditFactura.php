abe4dc3c77f986b5c7dae0d17dff661031da4d9b
nachobonilla6 <nachobonilla6@gmail.com>
2025-12-05 23:01:36 -0600

    Quitar botón close del footer en página de edición de facturas

--- app/Filament/Resources/FacturaResource/Pages/EditFactura.php
+++ app/Filament/Resources/FacturaResource/Pages/EditFactura.php
@@ -16,4 +16,11 @@ class EditFactura extends EditRecord
             Actions\DeleteAction::make(),
         ];
     }
+
+    protected function getFormActions(): array
+    {
+        return [
+            $this->getSaveFormAction(),
+        ];
+    }
 }

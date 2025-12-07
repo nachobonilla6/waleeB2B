153feb3e936ec165c7685c457513d7f2a3472192
nachobonilla6 <nachobonilla6@gmail.com>
2025-12-06 20:40:14 -0600

    Agregar nombre de marca 'WALEE' en la configuración del panel de administración

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -34,6 +34,7 @@ class AdminPanelProvider extends PanelProvider
             ->default()
             ->id('admin')
             ->path('admin')
+            ->brandName('WALEE')
             ->login()
             ->sidebarCollapsibleOnDesktop()
             ->darkMode(true)

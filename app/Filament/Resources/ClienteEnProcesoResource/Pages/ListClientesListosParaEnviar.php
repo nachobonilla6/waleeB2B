8395eebea49679596921a4f099265742f69c698b
nachobonilla6 <nachobonilla6@gmail.com>
2025-12-10 01:50:47 -0600

    Listos para enviar: misma UI que Clientes Google

--- app/Filament/Resources/ClienteEnProcesoResource/Pages/ListClientesListosParaEnviar.php
+++ app/Filament/Resources/ClienteEnProcesoResource/Pages/ListClientesListosParaEnviar.php
@@ -3,6 +3,7 @@
 namespace App\Filament\Resources\ClienteEnProcesoResource\Pages;
 
 use App\Filament\Resources\ClienteEnProcesoResource;
+use App\Filament\Resources\ClienteEnProcesoResource\Widgets\ClientesEnProcesoCards;
 use Filament\Resources\Pages\ListRecords;
 use Filament\Support\Enums\MaxWidth;
 use Illuminate\Database\Eloquent\Builder;
@@ -10,6 +11,13 @@ use Illuminate\Database\Eloquent\Builder;
 class ListClientesListosParaEnviar extends ListRecords
 {
     protected static string $resource = ClienteEnProcesoResource::class;
+
+    protected function getHeaderWidgets(): array
+    {
+        return [
+            ClientesEnProcesoCards::class,
+        ];
+    }
 
     public function getTitle(): string
     {

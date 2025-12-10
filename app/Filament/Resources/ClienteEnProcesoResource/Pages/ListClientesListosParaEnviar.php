c911c9810b517ca1395f10b4b4887c616ec0f078
nachobonilla6 <nachobonilla6@gmail.com>
2025-12-10 02:09:06 -0600

    Header/nav global DRY en todas las páginas de clientes google (botones iguales y resaltado activo)

--- app/Filament/Resources/ClienteEnProcesoResource/Pages/ListClientesListosParaEnviar.php
+++ app/Filament/Resources/ClienteEnProcesoResource/Pages/ListClientesListosParaEnviar.php
@@ -3,6 +3,7 @@
 namespace App\Filament\Resources\ClienteEnProcesoResource\Pages;
 
 use App\Filament\Resources\ClienteEnProcesoResource;
+use App\Filament\Resources\ClienteEnProcesoResource\Widgets\ClientesListosParaEnviarCards;
 use Filament\Resources\Pages\ListRecords;
 use Filament\Support\Enums\MaxWidth;
 use Illuminate\Database\Eloquent\Builder;
@@ -10,6 +11,13 @@ use Illuminate\Database\Eloquent\Builder;
 class ListClientesListosParaEnviar extends ListRecords
 {
     protected static string $resource = ClienteEnProcesoResource::class;
+
+    protected function getHeaderWidgets(): array
+    {
+        return [
+            ClientesListosParaEnviarCards::class,
+        ];
+    }
 
     public function getTitle(): string
     {
---
+++ app/Filament/Resources/ClienteEnProcesoResource/Widgets/ClientesListosParaEnviarCards.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Filament\Resources\ClienteEnProcesoResource\Widgets;
+
+use Filament\Widgets\Widget;
+
+class ClientesListosParaEnviarCards extends Widget
+{
+    protected static string $view = 'filament.resources.cliente-en-proceso-resource.widgets.clientes-listos-para-enviar-cards';
+}

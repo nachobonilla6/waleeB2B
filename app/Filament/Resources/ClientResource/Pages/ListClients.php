9b4dd24f3b91da5e19b6089f7b9d8545e5a255db
nachobonilla6 <nachobonilla6@gmail.com>
2025-12-13 13:31:36 -0600

    Set full width for clients table page

--- app/Filament/Resources/ClientResource/Pages/ListClients.php
+++ app/Filament/Resources/ClientResource/Pages/ListClients.php
@@ -5,10 +5,16 @@ namespace App\Filament\Resources\ClientResource\Pages;
 use App\Filament\Resources\ClientResource;
 use Filament\Actions;
 use Filament\Resources\Pages\ListRecords;
+use Filament\Support\Enums\MaxWidth;
 
 class ListClients extends ListRecords
 {
     protected static string $resource = ClientResource::class;
+
+    public function getMaxContentWidth(): MaxWidth | string | null
+    {
+        return MaxWidth::Full;
+    }
 
     protected function getHeaderActions(): array
     {

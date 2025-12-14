1dccc3f2b7a3d01a1c9d561b735864c6fd107dc7
nachobonilla6 <nachobonilla6@gmail.com>
2025-12-14 12:00:08 -0600

    Feature: Agregar página de documentación completa del sistema

---
+++ app/Filament/Pages/Documentacion.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Filament\Pages;
+
+use Filament\Pages\Page;
+
+class Documentacion extends Page
+{
+    protected static ?string $navigationIcon = 'heroicon-o-book-open';
+    protected static ?string $navigationLabel = 'Documentación';
+    protected static ?string $title = 'Documentación del Sistema';
+    protected static ?string $navigationGroup = 'Herramientas';
+    protected static ?int $navigationSort = 100;
+    protected static ?string $slug = 'documentacion';
+
+    protected static string $view = 'filament.pages.documentacion';
+
+    public static function shouldRegisterNavigation(): bool
+    {
+        return true;
+    }
+}

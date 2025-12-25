660c7ed19e3200681730341629191216726203c8
nachobonilla6 <nachobonilla6@gmail.com>
2025-12-25 12:29:21 -0600

    Agregar múltiples variaciones de campo label para asegurar que n8n aplique la etiqueta SUPPORT

--- routes/web.php
+++ routes/web.php
@@ -601,8 +601,14 @@ Route::post('/walee-tickets/{id}/estado', function (\Illuminate\Http\Request $re
                     'email_body' => "Hola {$nombre},\n\nSu ticket con el asunto: \"{$ticket->asunto}\" ha sido resuelto.\n\nGracias por contactarnos.\n\nSaludos,\nWeb Solutions",
                     'email_from' => 'websolutionscrnow@gmail.com',
                     'email_from_name' => 'Web Solutions',
+                    // Etiquetas para Gmail
                     'email_label' => 'SUPPORT',
                     'gmail_label' => 'SUPPORT',
+                    'label' => 'SUPPORT',
+                    'labels' => ['SUPPORT'],
+                    'labelIds' => ['SUPPORT'],
+                    'gmail_labels' => ['SUPPORT'],
+                    'addLabel' => 'SUPPORT',
                 ]
             );
         }

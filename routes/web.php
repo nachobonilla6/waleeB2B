445f43ac7fb239f1986310d91344734e0275a84d
nachobonilla6 <nachobonilla6@gmail.com>
2025-12-27 20:11:51 -0600

    Cambiar segunda instancia del webhook de test a producción

--- routes/web.php
+++ routes/web.php
@@ -2987,7 +2987,7 @@ Route::post('/walee-cliente/{id}/publicaciones/{publicacion_id}/republicar', fun
         }
         
         // Enviar al webhook de n8n
-        $webhookUrl = 'https://n8n.srv1137974.hstgr.cloud/webhook-test/692835c7-0e6a-4535-8d20-7b385a9a66ca';
+        $webhookUrl = 'https://n8n.srv1137974.hstgr.cloud/webhook/692835c7-0e6a-4535-8d20-7b385a9a66ca';
         
         $webhookData = [
             'texto' => $content,

b5ac533bb85d9f338cdd3f3e17a18b120d7b03a8
nachobonilla6 <nachobonilla6@gmail.com>
2026-01-03 21:49:53 -0600

    Cambiar botón guardar a amarillo walee y quitar icono de editar de la modal

--- resources/views/walee-cliente-detalle.blade.php
+++ resources/views/walee-cliente-detalle.blade.php
@@ -763,7 +763,7 @@
             }
             
             Swal.fire({
-                title: isMobile ? '' : '<svg class="w-6 h-6 text-walee-400 mx-auto" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z"/></svg>',
+                title: '',
                 html: html,
                 width: modalWidth,
                 padding: isMobile ? '0.75rem' : (isDesktop ? '1.25rem' : '1.5rem'),
@@ -779,6 +779,7 @@
                 showCancelButton: true,
                 confirmButtonText: 'Guardar',
                 cancelButtonText: 'Cancelar',
+                confirmButtonColor: '#D59F3B',
                 reverseButtons: true,
                 didOpen: () => {
                     // Aplicar tema dark/light al modal

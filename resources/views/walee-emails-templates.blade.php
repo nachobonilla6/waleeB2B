211aaf64d7eac56de92aeee88f7f59b569f579b2
nachobonilla6 <nachobonilla6@gmail.com>
2026-01-05 21:19:50 -0600

    Mostrar tipo de template junto al nombre en la lista de templates

--- resources/views/walee-emails-templates.blade.php
+++ resources/views/walee-emails-templates.blade.php
@@ -152,7 +152,12 @@
                                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M19.5 14.25v-2.625a3.375 3.375 0 00-3.375-3.375h-1.5A1.125 1.125 0 0113.5 7.125v-1.5a3.375 3.375 0 00-3.375-3.375H8.25m0 12.75h7.5m-7.5 3H12M10.5 2.25H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 00-9-9z"/>
                                 </svg>
                             </div>
-                            <h3 class="text-sm font-semibold text-slate-900 dark:text-white mb-1 line-clamp-1">{{ $template->nombre }}</h3>
+                            <h3 class="text-sm font-semibold text-slate-900 dark:text-white mb-1 line-clamp-1">
+                                {{ $template->nombre }}
+                                @if($template->tipo)
+                                    <span class="text-xs font-normal text-slate-500 dark:text-slate-400 ml-1">[{{ ucfirst($template->tipo) }}]</span>
+                                @endif
+                            </h3>
                             @if($template->tipo)
                                 <span class="inline-block px-2 py-0.5 text-xs font-medium rounded-md mb-2 
                                     @if($template->tipo === 'business') bg-blue-100 dark:bg-blue-500/20 text-blue-700 dark:text-blue-400

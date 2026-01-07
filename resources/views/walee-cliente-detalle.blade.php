c0ef2422975d312442651fde26f34fab8353ca93
nachobonilla6 <nachobonilla6@gmail.com>
2026-01-06 20:58:06 -0600

    Agregar texto 'Extractor' al botón del extractor

--- resources/views/walee-cliente-detalle.blade.php
+++ resources/views/walee-cliente-detalle.blade.php
@@ -676,6 +676,7 @@
                             <svg class="w-3.5 h-3.5 sm:w-4 sm:h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 6V4m0 2a2 2 0 100 4m0-4a2 2 0 110 4m-6 8a2 2 0 100-4m0 4a2 2 0 110-4m0 4v2m0-6V4m6 6v10m6-2a2 2 0 100-4m0 4a2 2 0 110-4m0 4v2m0-6V4"/>
                             </svg>
+                            <span class="text-xs sm:text-sm font-medium hidden sm:inline">Extractor</span>
                         </a>
                     </div>
                     
